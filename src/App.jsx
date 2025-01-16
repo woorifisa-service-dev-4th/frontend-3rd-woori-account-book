97c11914291831e49828fd92ba37f49ced7f3032
taegung <yesongmin12@gamil.com>
2025-01-16 09:37:43 +0900

    기초세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
 
 const App = () => {
+
+  const dummyData = [
+    {
+      id: 1,
+      date: 'React 공부',
+      type: 'React를 공부한다.',
+      text: 'TODO',
+      money: '1000'
+    },
+    {
+      id: 2,
+      date: 'React 공부',
+      type: 'React를 공부한다.',
+      text: 'TODO',
+      money: '1000'
+    },
+    {
+      id: 3,
+      date: 'React 공부',
+      type: 'React를 공부한다.',
+      text: 'TODO',
+      money: '1000'
+    },
+  ];
+
+
+
   return (
     <div>App</div>
   )
